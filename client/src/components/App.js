dfe137e7856ff53d545f37337da911b6daf5f817
Tejas Nampalli <TSNampalli@gmail.com>
2024-11-25 13:53:43 -0500

    refactor

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,37 +1,3 @@
-// import React from 'react';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import { AppProvider } from './AppContext';
-// import Navbar from './NavBar';
-// import Movies from './Movies';
-// import Rentals from './Rentals';
-// import Reviews from './Reviews';
-// import Home from './Home';
-// import Users from './Users';
-
-// function App() {
-//   return (
-//     <AppProvider>
-//       <Router>
-//         <Navbar />
-//         <div className="min-h-screen w-full px-4 py-4">
-//           <Routes>
-//             <Route path="/" element={<Home />} />
-//             <Route path="/movies" element={<Movies/>} />
-//             <Route path="/rentals" element={<Rentals />} />
-//             <Route path="/users" element={<Users />} />
-//             <Route path="/ratings" element={<Reviews />} />
-//           </Routes>
-//         </div>
-//       </Router>
-//     </AppProvider>
-//   );
-// }
-
-// export default App;
-
-
-
-
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AppProvider } from './AppContext';
